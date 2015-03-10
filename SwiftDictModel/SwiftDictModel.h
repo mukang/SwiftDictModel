be2fa22a7cf7958875ccc9766c26bd295fbc236f
mukang <mukang.max@foxmail.com>
2015-03-10 16:04:39 +0800

    实现了字典转模型功能

---
+++ SwiftDictModel/SwiftDictModel.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDictModel.h
+//  SwiftDictModel
+//
+//  Created by 穆康 on 15/3/10.
+//  Copyright (c) 2015年 穆康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDictModel.
+FOUNDATION_EXPORT double SwiftDictModelVersionNumber;
+
+//! Project version string for SwiftDictModel.
+FOUNDATION_EXPORT const unsigned char SwiftDictModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDictModel/PublicHeader.h>
+
+
